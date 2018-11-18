3969f2a479a4170f6b117e6c1e379bbf66d76235
Benjamin Soyka <37779854+bsoyka@users.noreply.github.com>
2018-11-18 08:10:24 -0800

    Update main.py

--- main.py
+++ main.py
@@ -7,4 +7,7 @@ bot = Bot(command_prefix="?")
 @bot.event
 async def on_ready():
     await bot.change_presence(game=discord.Game(name="tricks on your mind"))
+@bot.command(pass_context=True)
+async def ping(ctx):
+    await bot.say("Pong")
 bot.run(os.getenv("TOKEN"))
