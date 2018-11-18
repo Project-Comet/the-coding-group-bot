e297b83a1e6ca96afe953a80aca063d8c3cdc9e8
Benjamin Soyka <37779854+bsoyka@users.noreply.github.com>
2018-11-18 07:41:35 -0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+import discord
+from discord.ext import commands
+from discord.ext.commands import Bot
+import asyncio
+import os
+bot = Bot(command_prefix="$")
+@bot.event
+async def on_ready():
+    await bot.change_presence(game=discord.Game(name="tricks on your mind"))
+bot.run(os.getenv("TOKEN"))
