c613021244b636d172337422645308f05d8f9931
Benjamin Soyka <37779854+bsoyka@users.noreply.github.com>
2018-11-18 07:45:14 -0800

    Prefix change

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from discord.ext import commands
 from discord.ext.commands import Bot
 import asyncio
 import os
-bot = Bot(command_prefix="$")
+bot = Bot(command_prefix="?")
 @bot.event
 async def on_ready():
     await bot.change_presence(game=discord.Game(name="tricks on your mind"))
