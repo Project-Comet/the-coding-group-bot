d0fba7bc6bb756119b0be20d2d8f0770f8d6209f
Benjamin Soyka <37779854+bsoyka@users.noreply.github.com>
2018-11-18 17:10:39 -0800

    Update main.py

--- main.py
+++ main.py
@@ -9,6 +9,6 @@ async def on_ready():
     await bot.change_presence(game=discord.Game(name="with fire"))
 @bot.command(pass_context=True)
 async def ping(ctx):
-    """Sends a reply to show the bot is working."""
-    await bot.say("Pong")
+    """Sends a reply with the bot latency."""
+    await ctx.send('Pong! {0}'.format(round(bot.latency, 1))
 bot.run(os.getenv("TOKEN"))
