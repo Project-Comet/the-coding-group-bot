786d814d9f71f4a71d6d550835e1575b19f3c51b
Benjamin Soyka <37779854+bsoyka@users.noreply.github.com>
2018-11-18 19:11:17 -0800

    Add suggest command

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import time
 bot = Bot(command_prefix="?")
 @bot.event
 async def on_ready():
-    await bot.change_presence(game=discord.Game(name="Fortnite, which is bad"))
+    await bot.change_presence(game=discord.Game(name=""))
 @bot.command(pass_context=True)
 async def ping(ctx):
     """Sends a reply with the bot latency."""
@@ -20,4 +20,8 @@ async def punch(ctx, user: discord.Member):
     """Punches the specified user."""
     await bot.say("Now punching " + user.mention)
     await bot.send_message(user, "You've been punched!")
+@bot.command()
+async def suggest(*, msg: str):
+    channel=bot.get_channel('513579486608883724')
+    await bot.send_message(channel,msg)
 bot.run(os.getenv("TOKEN"))
