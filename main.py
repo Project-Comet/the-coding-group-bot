204ea086e74b207d3b07e3cd98c8f810f265062b
Benjamin Soyka <37779854+bsoyka@users.noreply.github.com>
2018-11-19 07:18:53 -0800

    Update main.py

--- main.py
+++ main.py
@@ -38,9 +38,5 @@ async def clear(ctx, amount=100):
     async for message in bot.logs_from(channel, limit=amount):
         messages.append(message)
     await bot.delete_messages(messages)
-@clear.error
-async def clear_error(error, ctx):
-    if isinstance(error, commands.CheckFailure):
-        await client.send_message(ctx.message.channel, "Looks like you don't have the permission to use that command, {}.".format(ctx.message.author.mention))
 bot.loop.create_task(change_status())
 bot.run(os.getenv("TOKEN"))
