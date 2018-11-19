1469cccb1866ea9254864ecd9eb71f8a847d1c8f
Benjamin Soyka <37779854+bsoyka@users.noreply.github.com>
2018-11-18 17:09:24 -0800

    Update main.py

--- main.py
+++ main.py
@@ -6,11 +6,7 @@ import os
 bot = Bot(command_prefix="?")
 @bot.event
 async def on_ready():
-    while True:
-        await bot.change_presence(game=discord.Game(name="with fire"))
-        asyncio.sleep(5)
-        await bot.change_presence(game=discord.Game(name="tricks on your mind"))
-        asyncio.sleep(5)
+    await bot.change_presence(game=discord.Game(name="with fire"))
 @bot.command(pass_context=True)
 async def ping(ctx):
     """Sends a reply to show the bot is working."""
