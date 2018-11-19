ebe93b41d66ac258b9a24fb37ed4263197400407
Benjamin Soyka <37779854+bsoyka@users.noreply.github.com>
2018-11-18 17:13:12 -0800

    Update main.py

--- main.py
+++ main.py
@@ -10,5 +10,5 @@ async def on_ready():
 @bot.command(pass_context=True)
 async def ping(ctx):
     """Sends a reply with the bot latency."""
-    await ctx.send('Pong! {0}'.format(round(bot.latency, 1))
+    await bot.say('Pong! {0}'.format(round(bot.latency, 1))
 bot.run(os.getenv("TOKEN"))
