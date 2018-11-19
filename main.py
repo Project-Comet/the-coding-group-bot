1014eab6d5fc2aa598e070942a54153906f30ccb
Benjamin Soyka <37779854+bsoyka@users.noreply.github.com>
2018-11-18 17:20:51 -0800

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from discord.ext import commands
 from discord.ext.commands import Bot
 import asyncio
 import os
+import time
 bot = Bot(command_prefix="?")
 @bot.event
 async def on_ready():
@@ -10,5 +11,10 @@ async def on_ready():
 @bot.command(pass_context=True)
 async def ping(ctx):
     """Sends a reply with the bot latency."""
-    await bot.say("Pong")
+    await bot.delete_message(ctx.message)
+    before = time.monotonic()
+    message = await ctx.send("Pong!")
+    ping = (time.monotonic() - before) * 1000
+    await message.edit(content=f"Pong!  `{int(ping)}ms`")
+    print(f'Ping {int(ping)}ms')
 bot.run(os.getenv("TOKEN"))
